809a3e298153c4c713c99d8abc66b47c354f5e58
t.carluy <t.carluy@isartdigital.com>
2024-08-19 11:35:55 +0200

    UI V2

--- Confiture/Assets/Scripts/UI/Followcursor.cs
+++ Confiture/Assets/Scripts/UI/Followcursor.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.UI;
 
 public class Followcursor : MonoBehaviour
 {
     public Vector3 Offset;
-    [SerializeField] TextMeshProUGUI BN_Text;
     public int BN_number;
     public PlayerEntity PE;
+    public GameObject cursor;
+
+    public Image BN1;
+    public Image BN2;
+    public Image BN3;
 
     public void Awake()
     {
@@ -19,6 +23,28 @@ public class Followcursor : MonoBehaviour
         Vector2 cursorPos = Input.mousePosition + Offset;
         transform.position = new Vector2(cursorPos.x, cursorPos.y);
 
-        BN_Text.text = PE.blobNumber.ToString();
+        BN_number = PE.blobNumber;
+
+        switch (BN_number)
+        {
+            case 1:
+                BN1.gameObject.SetActive(false);
+                BN2.gameObject.SetActive(false);
+                BN3.gameObject.SetActive(false);
+                break;
+            case 2:
+                BN1.gameObject.SetActive(true);
+                BN2.gameObject.SetActive(false);
+                BN3.gameObject.SetActive(false);
+                break;
+            case 3:
+                BN1.gameObject.SetActive(true);
+                BN2.gameObject.SetActive(true);
+                BN3.gameObject.SetActive(false);
+                break;
+            default:
+                Debug.LogWarning("currentValue doit être entre 1 et 3 !");
+                break;
+        }
     }
 }
